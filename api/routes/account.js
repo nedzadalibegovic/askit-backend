6a8e0af0ac81c268998bca454fcd67f6be9008cb
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-09-15 15:35:57 +0200

    Edit account info change route and add password change route

--- api/routes/account.js
+++ api/routes/account.js
@@ -3,12 +3,8 @@ const router = require("express").Router();
 const User = require("../../database/orm/models/User");
 const verifyAccessToken = require("../middlewares/verifyAccessToken");
 
-router.get("/:UserID", verifyAccessToken, async (req, res, next) => {
-  if (res.locals.UserID !== req.params.UserID) {
-    res.status(403).json({ message: "Not allowed" });
-    return;
-  }
-
+// this route should be used to get user's own info
+router.get("/", verifyAccessToken, async (req, res, next) => {
   try {
     const user = await User.query().findById(res.locals.UserID);
 
@@ -18,7 +14,7 @@ router.get("/:UserID", verifyAccessToken, async (req, res, next) => {
   }
 });
 
-// This route should be used to create (register) a new user
+// this route should be used to create (register) a new user
 router.post("/", async (req, res, next) => {
   try {
     const user = await User.query().insertAndFetch({
@@ -34,17 +30,46 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-// This route should be used to modify data of an existing user
+// this route should be used to modify data of an existing user
 router.put("/", verifyAccessToken, async (req, res, next) => {
   try {
-    const user = await User.query().patchAndFetchById(res.locals.UserID, {
+    const user = await User.query().findById(res.locals.UserID);
+
+    if (!(await user.verifyPassword(req.body.Password))) {
+      res.status(401).json({ message: "Incorrect password" });
+      return;
+    }
+
+    const result = await user.$query().patchAndFetch({
       FirstName: req.body.FirstName,
       LastName: req.body.LastName,
       Email: req.body.Email,
-      Password: req.body.Password,
     });
 
-    res.json(user);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// this route should be used to change the user's password
+router.put("/password", verifyAccessToken, async (req, res, next) => {
+  try {
+    const user = await User.query().findById(res.locals.UserID);
+
+    if (!(await user.verifyPassword(req.body.OldPassword))) {
+      res.status(401).json({ message: "Incorrect password" });
+      return;
+    }
+
+    const result = await user.$query().patch({
+      Password: req.body.NewPassword,
+    });
+
+    result > 0
+      ? res.json({ message: "Password updated" })
+      : res.status(500).json({ message: "Password not updated" });
+    return;
   } catch (err) {
     next(err);
   }
