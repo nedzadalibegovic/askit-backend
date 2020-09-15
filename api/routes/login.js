70e4eb3b03f7801d80207c1c8c1b4835c7d5c4dc
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-09-15 04:19:11 +0200

    Add logout route

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const sha256 = require("crypto-js/sha256");
+const cookieParser = require("cookie-parser");
+const { raw } = require("objection");
 
 const authenticate = require("../helpers/authenticate");
 const jwt = require("../helpers/jwt");
@@ -21,6 +23,25 @@ router.post("/", async (req, res, next) => {
       secure: process.env.NODE_ENV === "production" ? true : false,
     });
     res.json({ accessToken });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/", cookieParser(), async (req, res, next) => {
+  try {
+    const { refreshToken } = req.cookies;
+    const { UserID } = jwt.verifyRefreshToken(refreshToken);
+    const user = await User.query().findById(UserID);
+
+    if (user.Token === sha256(refreshToken).toString()) {
+      await user.$query().patch({ Token: raw("NULL") });
+
+      res.clearCookie("refreshToken");
+      res.json({ message: "You've been logged out" });
+    } else {
+      res.status(401).json({ message: "Please login again" });
+    }
   } catch (err) {
     next(err);
   }
